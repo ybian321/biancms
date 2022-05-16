c39bfa707ae958603b7f98a7317b684eae30f89c
BYJ <ybian321@gmail.com>
2022-05-16 20:00:33 +1000

    update breadcrumb

--- components/SmartBreadcrumb.tsx
+++ components/SmartBreadcrumb.tsx
@@ -1,30 +1,126 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { Breadcrumb } from 'antd';
-import { deepClone } from '../lib/util/deep-search';
+
+interface Route {
+   path: string;
+   breadcrumbName: string;
+   parentBreadcrumb?: string;
+   grandparent?: string;
+}
 
 function SmartBreadcrumb() {
-  const path = useRouter().asPath;
-  const paths = path.split('/').slice(1);
+   const path = useRouter().asPath;
+   const paths = path.split('/').slice(1);
 
-  const role = path.split('/').slice(2, 3);
-  const root = `/dashboard/` + role;
+   //Dashboard Path
+   const role = path.split('/').slice(2, 3).toString();
+   const root = `/dashboard/` + role;
 
-  const route = deepClone(paths).map((element: any) => ({
-    path: element,
-    breadcrumbName: element
-  }));
-  const routes = route;
+   //Role Breadcrumb Name
+   const roleName = role.replace(role[0], role[0].toUpperCase());
 
-  function itemRender(route: any, params: any, routes: any, paths: any) {
-    const isOverviewPage = paths.length <= 2;
-    const subPage = '/' + paths.join('/');
+   //third layer
+   const thirdLayer = path.split('/').slice(3, 4).toString();
 
-    const last = routes.indexOf(route) === routes.length - 1;
-    return last ? <span>{route.breadcrumbName}</span> : <Link href={isOverviewPage ? root : subPage}>{route.breadcrumbName}</Link>;
-  }
+   //forth layer
+   const forthLayer = path.split('/').slice(4, 5).toString();
 
-  return <Breadcrumb itemRender={itemRender} routes={routes} style={{ margin: '16px 0' }} />;
+   //last layer
+   const lastLayer = path.substring(path.lastIndexOf('/') + 1);
+
+   const routes: Route[] = [
+      {
+         path: root,
+         breadcrumbName: 'Overview'
+      },
+      {
+         path: root + '/students',
+         breadcrumbName: 'Student List',
+         parentBreadcrumb: 'Student'
+      },
+      {
+         path: root + '/students/' + forthLayer,
+         grandparent: 'Student',
+         parentBreadcrumb: 'Student List',
+         breadcrumbName: 'Detail'
+      },
+      {
+         path: root + '/teachers',
+         parentBreadcrumb: 'Teacher',
+         breadcrumbName: 'Teacher List'
+      },
+      {
+         path: root + '/courses',
+         parentBreadcrumb: 'Course',
+         breadcrumbName: 'All Courses'
+      },
+      {
+         path: root + '/courses/add-course',
+         parentBreadcrumb: 'Course',
+         breadcrumbName: 'Add Course'
+      },
+      {
+         path: root + '/courses/edit-course',
+         parentBreadcrumb: 'Course',
+         breadcrumbName: 'Edit Course'
+      }
+   ];
+
+   function deepSearch(path: string, data: Route[]) {
+      return data.map((item: any) => {
+         if (item.path === path && !forthLayer) {
+            // console.log(`[current path]`, path)
+            // console.log(`[breadcrumb name]`, item.breadcrumbName)
+            return (
+               <>
+                  <Breadcrumb.Item>{item.parentBreadcrumb}</Breadcrumb.Item>
+                  <Breadcrumb.Item>
+                     <Link href={root + '/' + thirdLayer}>
+                        <a>{item.breadcrumbName}</a>
+                     </Link>
+                  </Breadcrumb.Item>
+               </>
+            );
+         }
+         if (item.path === path && forthLayer) {
+            console.log(`[current path]`, path);
+            console.log(`[breadcrumb name]`, item.breadcrumbName);
+            return (
+               <>
+                  <Breadcrumb.Item>{item.grandparent}</Breadcrumb.Item>
+                  <Breadcrumb.Item>
+                     <Link href={root + '/' + thirdLayer}>
+                        <a>{item.parentBreadcrumb}</a>
+                     </Link>
+                  </Breadcrumb.Item>
+                  <Breadcrumb.Item>{item.breadcrumbName}</Breadcrumb.Item>
+               </>
+            );
+         }
+      });
+   }
+
+   function setBreadcrumb() {
+      const isOverviewPage = paths.length <= 2;
+
+      if (isOverviewPage) {
+         return <Breadcrumb.Item>Overview</Breadcrumb.Item>;
+      } else {
+         return <>{deepSearch(path, routes)}</>;
+      }
+   }
+
+   return (
+      <Breadcrumb style={{ margin: '16px 0' }}>
+         <Breadcrumb.Item>
+            <Link href={root}>
+               <a>CMS MANAGER SYSTEM</a>
+            </Link>
+         </Breadcrumb.Item>
+         {setBreadcrumb()}
+      </Breadcrumb>
+   );
 }
 
 export default SmartBreadcrumb;
