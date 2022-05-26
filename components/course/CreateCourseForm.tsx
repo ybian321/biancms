eda975dbf4e61d032a20a29c79ab2e5ecb7bf793
BYJ <ybian321@gmail.com>
2022-05-26 23:08:38 +1000

    update add course form

--- components/course/CreateCourseForm.tsx
+++ components/course/CreateCourseForm.tsx
@@ -1,18 +1,19 @@
-import { useState } from 'react';
-import { Button, Col, DatePicker, Form, Input, InputNumber, Row, Select, Upload } from 'antd';
-import { Teacher } from '../../lib/types/teachers.type';
+import { Col, DatePicker, Form, Input, InputNumber, Row, Select } from 'antd';
 import TextArea from 'antd/lib/input/TextArea';
 import Dragger from 'antd/lib/upload/Dragger';
 import { InboxOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
+import { Course } from '../../lib/types/students.type';
 
+export interface AddCourseFormProps {
+   course?: Course;
+   onSuccess?: (course: Course) => void;
+}
+
+/**
+ * reset antd style
+ */
 const DescriptionTextArea = styled(Form.Item)`
-   .ant-form-item-control {
-      position: absolute;
-      inset: 0;
-      top: 37px;
-      bottom: 30px;
-   }
    .ant-form-item-control-input,
    .ant-form-item-control-input-content,
    text-area {
@@ -20,61 +21,69 @@ const DescriptionTextArea = styled(Form.Item)`
    }
 `;
 
-export default function CreateCourseForm() {
-   const [teachers, setTeachers] = useState<Teacher[]>([]);
+const UploadItem = styled(Form.Item)`
+   .ant-form-item-control-input,
+   .ant-form-item-control-input div {
+      height: 100%;
+   }
+`;
 
+export default function CreateCourseForm() {
    const onFinish = (values: any) => {
       console.log('Success:', values);
    };
 
-   const onFinishFailed = (errorInfo: any) => {
-      console.log('Failed:', errorInfo);
-   };
+   const selectAfter = (
+      <Select defaultValue="month" className="select-after">
+         <Select.Option value="year">year</Select.Option>
+         <Select.Option value="month">month</Select.Option>
+         <Select.Option value="day">day</Select.Option>
+         <Select.Option value="week">week</Select.Option>
+         <Select.Option value="hour">hour</Select.Option>
+      </Select>
+   );
 
    return (
-      <Form layout="vertical" onFinish={onFinish} onFinishFailed={onFinishFailed}>
-         <Row>
-            <Col span={8}>
+      <Form layout="vertical" onFinish={onFinish}>
+         <Row className="flex justify-between">
+            <Col span={8} style={{ marginRight: 15 }}>
                <Form.Item label="Course Name" name="name" rules={[{ required: true }, { max: 100, min: 3 }]}>
                   <Input type="text" placeholder="course name" />
                </Form.Item>
             </Col>
-            <Col span={16}>
-               <Row>
-                  <Col span={8}>
-                     <Form.Item label="Teacher" name="teacherId" rules={[{ required: true }]} style={{ marginLeft: 5 }}>
-                        <Select placeholder="Select teacher">
-                           {/* {teachers.map(({ id, name }) => (
-                              <Select.Option key={id} value={id}>
-                                 {name}
-                              </Select.Option>
-                           ))} */}
-                        </Select>
-                     </Form.Item>
-                  </Col>
 
-                  <Col span={8}>
-                     <Form.Item label="Type" name="type" rules={[{ required: true }]} style={{ marginLeft: 5 }}>
-                        <Select mode="multiple">
-                           {/* {courseTypes.map((type) => (
-                              <Select.Option value={type.id} key={type.id}>
-                                 {type.name}
-                              </Select.Option>
-                           ))} */}
-                        </Select>
-                     </Form.Item>
-                  </Col>
+            <Col span={5}>
+               <Form.Item label="Teacher" name="teacherId" rules={[{ required: true }]}>
+                  <Select placeholder="Select teacher">
+                     {/* {teachers.map(({ id, name }) => (
+                        <Select.Option key={id} value={id}>
+                           {name}
+                        </Select.Option>
+                     ))} */}
+                  </Select>
+               </Form.Item>
+            </Col>
 
-                  <Col span={8}>
-                     <Form.Item label="Course Code" name="uid" rules={[{ required: true }]} style={{ marginLeft: 5 }}>
-                        <Input type="text" placeholder="course code" disabled />
-                     </Form.Item>
-                  </Col>
-               </Row>
+            <Col span={5}>
+               <Form.Item label="Type" name="type" rules={[{ required: true }]}>
+                  <Select mode="multiple">
+                     {/* {courseTypes.map((type) => (
+                        <Select.Option value={type.id} key={type.id}>
+                           {type.name}
+                        </Select.Option>
+                     ))} */}
+                  </Select>
+               </Form.Item>
+            </Col>
+
+            <Col span={5}>
+               <Form.Item label="Course Code" name="uid" rules={[{ required: true }]}>
+                  <Input type="text" placeholder="course code" disabled />
+               </Form.Item>
             </Col>
          </Row>
 
-         <Row>
+         <Row className="flex justify-between">
             <Col span={8}>
                <Form.Item label="Start Date" name="startTime">
                   <DatePicker
@@ -102,22 +111,14 @@ export default function CreateCourseForm() {
                   <InputNumber min={1} max={10} style={{ width: '100%' }} />
                </Form.Item>
 
-               <Form.Item
-                  label="Duration"
-                  name="duration"
-                  // rules={[{ required: true }, { validator: validateDuration }]}
-               >
-                  {/* <NumberWithUnit
-                     options={new Array(5)
-                        .fill('')
-                        .map((_, index) => ({ unit: index + 1, label: DurationUnit[index + 1] }))}
-                     defaultUnit={DurationUnit.month}
-                  /> */}
+               <Form.Item style={{ marginBottom: 0 }} label="Duration" name="duration">
+                  <InputNumber addonAfter={selectAfter} style={{ width: '100%' }} />
                </Form.Item>
             </Col>
 
-            <Col span={8} style={{ position: 'relative', marginLeft: 5 }}>
+            <Col span={8} style={{ paddingLeft: 20 }}>
                <DescriptionTextArea
+                  style={{ height: '100%' }}
                   label="Description"
                   name="detail"
                   rules={[
@@ -133,22 +134,20 @@ export default function CreateCourseForm() {
                </DescriptionTextArea>
             </Col>
 
-            <Col span={8} style={{ marginLeft: 5 }}>
-               <Dragger>
-                  <p className="ant-upload-drag-icon">
-                     <InboxOutlined />
-                  </p>
-                  <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                  <p className="ant-upload-hint">
-                     Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files
-                  </p>
-               </Dragger>
+            <Col span={8} style={{ paddingLeft: 20 }}>
+               <UploadItem label="Cover" style={{ height: '100%' }}>
+                  <Dragger>
+                     <p className="ant-upload-drag-icon">
+                        <InboxOutlined />
+                     </p>
+                     <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                     <p className="ant-upload-hint">
+                        Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files
+                     </p>
+                  </Dragger>
+               </UploadItem>
             </Col>
          </Row>
-
-         <Button type="primary" htmlType="submit">
-            Create Course
-         </Button>
       </Form>
    );
 }
